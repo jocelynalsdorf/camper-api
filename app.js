6481e8aaa8731f9a0768a4db5f27cd4fbe102986
jocelynalsdorf <jfrodsla@gmail.com>
2015-11-07 10:37:19 -0800

    add api call and append html and data to view

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
-
-  $window = $(window);
- 
- // $('.grid').masonry({
- // // options
- // itemSelector: '.grid-item',
- 
- 
- // });
+var html = "";
+  $.getJSON( "http://www.freecodecamp.com/news/hot", function( data ) {
+  
+  $.each(data, function( key, value ) {
+    // console.log(value.headline );
+    // console.log(value.image );
+    // console.log(value.rank );
+    // console.log(value.link );
+    // console.log(value.author.picture );
+    // console.log(value.author.username);
+    html += "<div class='mdl-card mdl-shadow--4dp grid-item'><div class='mdl-card__media'><img src='" + value.image + "' width='173' height='157' border='0' style='padding:10px;'></div><div class='mdl-card__supporting-text'>" + value.headline + "</div><div class='mdl-card__supporting-text'>" + value.rank + value.author.username + "</div></div>";
+    $("#content-holder").html(html);
 
 });
+  
+  
+});
+
+});
+
