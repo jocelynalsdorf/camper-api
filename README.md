d581f48ca2655c2912f222fff945abddc8bb5e7c
jocelynalsdorf <jfrodsla@gmail.com>
2015-11-07 12:05:46 -0800

    fix col-width issue; add readme, fix indentation

---
+++ README.md
@@ -0,0 +1,23 @@
+#Free Code Camp Camper Articles API
+<h3>Author:</h3>
+Jocelyn Alsdorf
+
+<h3>Description:</h3>
+Just Another Free Code Camp Project: Use the FCC API to get all 'hot' articles and display appropriate info about the article and author
+
+<h2>Dependencies:</h2>
+
+material design lite
+JQuery
+API: FCC
+
+
+
+---------
+## Site home screenshot
+
+![Screenshot of site] (https://raw.githubusercontent.com/jocelynalsdorf/camper-api/master/camper-screenshot.png)
+
+
+<h2>Copyright</h2>
+MIT License. Copyright 2015  Jocelyn Alsdorf
--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 $(document).ready(function(){
-var html = "";
+  var html = "";
   $.getJSON( "http://www.freecodecamp.com/news/hot", function( data ) {
   
-  $.each(data, function( key, value ) {
-    // console.log(value.headline );
-    // console.log(value.image );
-    // console.log(value.rank );
-    // console.log(value.link );
-    // console.log(value.author.picture );
-    // console.log(value.author.username);
-    html += "<div class='mdl-card mdl-shadow--4dp grid-item'><div class='mdl-card__media'><img src='" + value.image + "' width='173' height='157' border='0' style='padding:10px;'></div><div class='mdl-card__supporting-text'>" + value.headline + "</div><div class='mdl-card__supporting-text'>" + value.rank + value.author.username + "</div></div>";
-    $("#content-holder").html(html);
+    $.each(data, function( key, value ) {
 
-});
-  
-  
-});
+      html += "<div class='mdl-card mdl-shadow--4dp grid-item'><div class='mdl-card__media'><div class='img-container'><img class='img-article' src='" + value.author.picture + "' width='175' height='175' border='0' style='padding:10px;'></div></div><div class='mdl-card__supporting-text'>" + value.headline + "</div><div class='mdl-card__supporting-text'><span class='highlight'>Upvotes: </span>" + value.rank + " <span class='highlight'>By:  </span><a href='http://www.freecodecamp.com/"+ value.author.username +"'>@" + value.author.username + "</a><button type='button' class='mdl-button mdl-js-button mdl-button--raised mdl-button--colored link-btn'><a href='" + value.link + "'>See More</a></button></div></div>";
+      $("#content-holder").html(html);
 
+    });
+  });
 });
 
